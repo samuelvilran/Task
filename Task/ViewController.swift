688edeb6c7dcfb1723dc93aa9eb137e31281b785
Samuel Villanueva Rangel <samuel.vilran@gmail.com>
2023-10-30 10:49:02 -0600

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Mac on 30/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
